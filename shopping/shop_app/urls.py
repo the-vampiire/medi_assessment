a8a2a58cce8d0a46316d3d70c5a960b706c2d27f
The-Vampiire <vampiirecodes@gmail.com>
2018-03-22 16:53:02 -0400

    Created Category List view
    - configured templates
    - created base template with nav and footer
    - created Category List view that extends base template
    - simple product listing for now. will improve in next branch

--- shopping/shop_app/urls.py
+++ shopping/shop_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, category
 
 app_name = 'shop_app'
 
 urlpatterns = [
-  path('', index, name = 'index')
+  path('', index, name = 'index'),
+  path('category/<int:category_id>', category, name = 'category')
 ]
--- shopping/shop_app/views.py
+++ shopping/shop_app/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Category, Product
 
 def index(request):
-  return HttpResponse('base configuration complete')
+  context = {
+    "title": "All Products",
+    "tab_categories": Category.objects.all()
+  }
+  return render(request, 'base.html', context)
+
+def category(request, category_id):
+  active_category = Category.objects.get(pk = category_id)
+  context = {
+    "title": "%s products" % active_category.name,
+    "tab_categories": Category.objects.all(),
+    "active_category": category_id,
+    "products": active_category.product_set.all(),
+  }
+  return render(request, 'shop_app/category_list.html', context)

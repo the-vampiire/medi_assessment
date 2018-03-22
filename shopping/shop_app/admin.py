733f47af394ecb29149e07178e63d07ec178edc4
The-Vampiire <vampiirecodes@gmail.com>
2018-03-21 22:52:12 -0400

    added models to admin panel
    - update the verbose_name and verbose_name_plural for the Category model

--- shopping/shop_app/admin.py
+++ shopping/shop_app/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Category, Product, Customer, Address, Receipt
 
-# Register your models here.
+admin.site.register([Category, Product, Customer, Address, Receipt])

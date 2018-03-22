53533716a25c0dd34605e15fc07cf5faf172c573
The-Vampiire <vampiirecodes@gmail.com>
2018-03-21 22:09:19 -0400

    create Addresses table and migration

---
+++ shopping/shop_app/models/address.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Address(models.Model):
+  class Meta:
+    db_table = 'addresses'
+
+  street = models.CharField(max_length = 255, blank = False)
+  zipcode = models.CharField(max_length = 5, blank = False)
+  city = models.CharField(max_length = 30, blank = False)
+  state = models.CharField(max_length = 13, blank = False)
+  

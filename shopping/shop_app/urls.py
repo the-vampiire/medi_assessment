8a073f7902d7d215ea124590c9b3c1ac0779d45c
The-Vampiire <vampiirecodes@gmail.com>
2018-03-23 07:17:49 -0400

    Completed Purchase endpoint

--- shopping/shop_app/urls.py
+++ shopping/shop_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import index, category, product
+from .views import index, category, product, purchase
 
 app_name = 'shop_app'
 
 urlpatterns = [
   path('', index, name = 'index'),
   path('category/<int:category_id>', category, name = 'category'),
-  path('products/<int:product_id>', product, name = 'product')
+  path('products/<int:product_id>', product, name = 'product'),
+  path('products/<int:product_id>/purchase', purchase, name = 'purchase')
 ]
